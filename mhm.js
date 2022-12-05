7e56c7f06bc8243fc2b13cc894495073ad75c76d
ripoffuser <76708923+ripoffuser@users.noreply.github.com>
2022-12-04 21:44:52 -0500

    Add files via upload

---
+++ mhm.js
@@ -0,0 +1,6 @@
+let logger='working';
+const Logger={
+  log(e){console.log('%cNullify:'+e,'color:white;')},
+  warn(e){console.log('%cNullify:'+e,'color:yellow;')},
+  error(e){console.log('%cNullify:'+e,'color:red;')}
+}
